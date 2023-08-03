62973c3180604e381002e5b21d8173503321020c
eqmccullough <132762410+eqmccullough@users.noreply.github.com>
2023-08-03 15:25:56 -0400

    Update AboutMe.js

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -5,7 +5,7 @@ export default function AboutMe() {
   return (
     <div>
       <h1>About Me</h1>
-      <img class="col-sm-1" src={evBrisk} alt="Picture of Evan McCullough" />
+      <img class="col-sm-1" src={evBrisk} alt="Evan McCullough" />
 
       <p>
         My name is Evan McCullough, I currently work as a tennis coach and personal trainer. However, I was educated and worked as a mechanical engineer. Now I am looking to make a change and enter the tech industry.
